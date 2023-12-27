046c7b6553333b8ffc08ca064a3d4f5200edc495
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2023-12-27 12:21:42 -0500

    Remove Mogrify

--- resources/build_resources.sh
+++ resources/build_resources.sh
@@ -12,8 +12,6 @@
 
 PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python"
 
-# Remove any problematic profiles from pngs.
-for f in *.png; do mogrify $f; done
 
 # Helper functions to build UI files
 function build_qt {
